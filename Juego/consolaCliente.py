914ddad0a9f33a8cd3ba1fb3cea07d7c22be01ef
nbmateus <mateusnicolasblas@gmail.com>
2018-11-23 15:25:06 -0300

    consola cliente

--- Juego/consolaCliente.py
+++ Juego/consolaCliente.py
@@ -2,24 +2,80 @@ import socket
 import os
 import time
 import cliente
+from Crypto.Cipher import AES
+import base64
+import json
 
+secret_key = 'a15fg7s9h75q17a8'.encode()
+
+def checkJSON(jObjeto):
+    try:
+        dato = json.loads(jObjeto)
+        cantidad = 0
+        debeHaber = 0
+        for i in dato:
+            if(cantidad == 0):
+                debeHaber = dato[i]
+            cantidad = cantidad + 1
+        if(cantidad - 1 != debeHaber):
+            return False
+        return True
+    except ValueError:
+        print("No soy jayson")
+        return False
+
+def encriptar(mensaje):
+    mensaje = mensaje.rjust(768)
+    cipher = AES.new(secret_key, AES.MODE_ECB)
+    msjCriptado = base64.b64encode(cipher.encrypt(mensaje.encode()))
+    if(len(msjCriptado) > 1024):
+        raise Exception("La encriptacion sobrepaso los 1024 bytes")
+    return msjCriptado
+
+
+def desencriptar(mensajeEncriptado):
+    cipher = AES.new(secret_key, AES.MODE_ECB)
+    return cipher.decrypt(base64.b64decode(mensajeEncriptado)).strip().decode()
 
 def run_consola():
 	sock = cliente.crearConexion()
-	sock.sendall("Lista de comandos de consola".encode())
-	listaDeComandos = sock.recv(1000).decode()
 	salir = False
 	while salir is not True:
-		print(listaDeComandos)
 		print("\nIngrese un comando: ")
 		comando = input()
-		print("\ncomando enviado: ", str(comando))
-		sock.sendall(str(comando).encode())
-		dato = sock.recv(450).decode()
-		print("\ndato recibido: ", str(dato))
+		"""
+		comando = comando[1:]
+		comando = comando[:-1]
+
+		dicComando = {}
+
+		print("este es el comando: ",comando)
+		comandoSpliteado = comando.split(",")
+		cnt = 0
+		for i in comandoSpliteado:
+			iSpliteado = i.split(":")
+			print(iSpliteado)
+			if(cnt == 0):
+				dicComando[iSpliteado[0]] = int(iSpliteado[1])
+			else:
+				dicComando[iSpliteado[0]] = iSpliteado[1]
+
+			cnt = cnt + 1
+		dicComando2 = json.dumps(dicComando)
+		print("diccomando: ",dicComando)
+
+
+		"""
+		sock.sendall(encriptar(comando))
+		dato = desencriptar(sock.recv(1024))
+		if(checkJSON(dato)):
+			datoConvertido = json.loads(dato)
+		print("\ndato recibido: ", datoConvertido)
 		if(comando=="salir"):
 			sock.close()
 			salir = True
+
+
 
 		
 if __name__ == '__main__':
